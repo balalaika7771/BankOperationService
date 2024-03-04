414c40e8e29b7afcc4b45a8244e5e5e3793e0aa3
i_zhendorenko <ya.m.r-rice@yandex.ru>
2024-03-04 11:06:03 +0300

    generate

---
+++ src/main/java/com/test/BankOperationService/BankOperationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.test.BankOperationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOperationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankOperationServiceApplication.class, args);
+	}
+
+}
